db7674ccc7d3f1c5de1257f1114c386e168a4a79
DiegoKraenau <diegokraenau@gmail.com>
2023-11-03 22:25:01 -0500

    fix

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,12 +1,12 @@
 // Import the required modules using ESM syntax
-const path = require('path');
+const path = require("path");
 
 // Export the configuration object
 module.exports = {
   apps: [
     {
       name: "back",
-      script: path.join(process.cwd(), 'node_modules', '.bin', 'npm'),
+      script: "npm",
       args: "run prod",
     },
   ],
